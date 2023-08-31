b7b43fd0d06842e9b1bd4a8c0a72a223deba0b90
Sebastian <carefulsniffle@gmail.com>
2023-08-31 12:50:41 -0500

    Double check my work please

--- GAMEFILES/crafting.py
+++ GAMEFILES/crafting.py
@@ -2,13 +2,16 @@ import inventory
 
 recipes_blueprints = {("metal spear"):("rope","handle","metal scrap")}
 
+attempt = 0
+
 inv = inventory.Inventory()
 inv.add_item("rope")
 inv.add_item("handle")
 inv.add_item("metal scrap")
 
-if(inv.contains_item(recipes_blueprints["metal spear"])):
-    inv.add_item("metal spear")
-    print(inv)
-else:
-    print("Error")
+for required_item in recipes_blueprints["metal spear"]:
+    if inv.contains_item(required_item):
+        attempt + 1
+    if attempt == len(recipes_blueprints["metal spear"]):
+        attempt = 0
+        inv.add_item("metal_spear")
