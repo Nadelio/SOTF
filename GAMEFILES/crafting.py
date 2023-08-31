bb70f08173ee87e409ab33f0edff7ef960dbe0ca
Sebastian <carefulsniffle@gmail.com>
2023-08-31 13:32:41 -0500

    Created basic template for new crafting system

--- GAMEFILES/crafting.py
+++ GAMEFILES/crafting.py
@@ -8,16 +8,16 @@ inv.add_item("rope")
 inv.add_item("rope")
 inv.add_item("handle")
 inv.add_item("metal scrap")
+def crafting_attempt():
+    for required_item in recipes_blueprints["metal spear"]:
+        if inv.contains_item(required_item):
+            attempt += 1
+            inv.remove_item(required_item)
+        print(attempt)
 
-for required_item in recipes_blueprints["metal spear"]:
-    if inv.contains_item(required_item):
-        attempt += 1
-        inv.remove_item(required_item)
-    print(attempt)
-
-print(len(recipes_blueprints["metal spear"]))
-if attempt == len(recipes_blueprints["metal spear"]):
-    attempt = 0
-    inv.add_item("metal_spear")
-    for item in inv.items:
-        print(item)
+    print(len(recipes_blueprints["metal spear"]))
+    if attempt == len(recipes_blueprints["metal spear"]):
+        attempt = 0
+        inv.add_item("metal_spear")
+        for item in inv.items:
+            print(item)
