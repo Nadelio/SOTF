908d1d38721a417b02c918a591389a0beb181451
Sebastian <carefulsniffle@gmail.com>
2023-09-07 13:46:06 -0500

    Nadelio
    Bug Fixes

--- GAMEFILES/enemy_construct.py
+++ GAMEFILES/enemy_construct.py
@@ -23,7 +23,7 @@ class Zombie(Mob):
 
 class Animal(Mob):
     def __init__(self):
-        super().__init__(mob_tag = Mobtypes[1])
+        super().__init__(mob_tag = Mobtypes[3])
         super().__init__(health = random.randrange(2)+1)
         super().__init__(damage = 1)
         super().__init__(defense = 0)
@@ -37,7 +37,7 @@ class Bandit(Mob):
 
 class Skeleton:
     def __init__(self):
-        super().__init__(mob_tag = Mobtypes[3])
+        super().__init__(mob_tag = Mobtypes[1])
         super().__init__(health = random.randrange(1)+1)
         super().__init__(damage = random.randrange(1)+1)
         super().__init__(defense = 0)
@@ -48,24 +48,24 @@ def main(poi):
     mob_spawn = poi
     match mob_spawn:
         case 1:#city
-            mob = Zombie()
+            mob = Zombie(Mobtypes[0], random.randrange(4)+1, random.randrange(1)+1, 0)
             print(f'Enemy type: {mob.mob_tag}')
             return mob
 
         case 2:#forest
-            mob = Animal()
+            mob = Animal(Mobtypes[3], random.randrange(2)+1, 1, 0)
             print(f'Enemy type: {mob.mob_tag}')
             return mob
-        case 3:#catacombs
-            mob = Skeleton()
+        case 3:#catacombs #
+            mob = Skeleton(Mobtypes[1], random.randrange(1)+1, random.randrange(1)+1, 0)
             print(f'Enemy type: {mob.mob_tag}')
             return mob
         case 4:#UNUSED
-            mob = Bandit()
+            mob = Bandit(Mobtypes[2], random.randrange(1)+4, random.randrange(2)+1, 1)
             print(f'Enemy type: {mob.mob_tag}')
             return mob
         case _:#error
-            mob = Mob()
+            mob = Mob("Default Mob", 1, 1, 0)
             print(f'Enemy type: {mob.mob_tag}')
             return mob
 
